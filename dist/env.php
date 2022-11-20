f869cf642572bfcff2c671abf831c3ed38df4533
serhanif59 <57870623+serhanif59@users.noreply.github.com>
2022-11-20 22:09:47 +0100

    Update env.php

--- dist/env.php
+++ dist/env.php
@@ -1,6 +1,6 @@
 <?php
-define('APP_DB_USER', 'admin');
-define('APP_DB_PASSWORD', 'toto5638');
+define('APP_DB_USER', '');
+define('APP_DB_PASSWORD', '');
 define('APP_DB_HOST', 'localhost');
 define('APP_DB_PORT', '3306');
 define('APP_DB_NAME', 'newDatabase');
